fb4af0eb0b0222872f821f2722ac41d8f6365b2d
Tochukwu <nwibetochukwu@gmail.com>
2024-12-09 13:25:12 +0000

    Advanced Django Models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
-from .views import list_books
+from .views import LoginView, LogoutView, book_list_view, LibraryDetailView
 
 urlpatterns = [
     path('books/list-of-books/', view=views.book_list_view),
     path('library/library-detail/<int:pk>/', views.LibraryDetailView.as_view()),
 
     #url paths for authentication and registration
-    path('accounts/login/', views.LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
+    path('accounts/login/', LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
     path('accounts/register/', views.register.as_view(template_name = 'relationship_app/register.html'), name='register')
 ]
