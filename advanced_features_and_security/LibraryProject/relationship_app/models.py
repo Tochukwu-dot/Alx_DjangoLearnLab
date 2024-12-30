3bfcf7948e5eb6dde8000cc1f9d79c0f676d64f8
Tochukwu <nwibetochukwu@gmail.com>
2024-12-30 05:37:26 +0000

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -39,46 +39,46 @@ class Librarian(models.Model):
 
     
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email,username, date_of_birth, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, date_of_birth = date_of_birth, **extra_fields)
-        user.set_password(password)
-        user.save(using = self._db)
-        return user
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email,username, date_of_birth, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError('The Email field must be set')
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, username=username, date_of_birth = date_of_birth, **extra_fields)
+#         user.set_password(password)
+#         user.save(using = self._db)
+#         return user
     
-    def create_super_user(self, email, username, date_of_birth, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+#     def create_super_user(self, email, username, date_of_birth, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError ('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, username, date_of_birth, password, **extra_fields)
-
-
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
+#         if extra_fields.get('is_staff') is not True:
+#             raise ValueError ('Superuser must have is_staff=True.')
+#         if extra_fields.get('is_superuser') is not True:
+#             raise ValueError('Superuser must have is_superuser=True.')
+#         return self.create_user(email, username, date_of_birth, password, **extra_fields)
 
 
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+# class CustomUser(AbstractUser):
+#     email = models.EmailField(unique=True)
+#     date_of_birth = models.DateField(null=True, blank=True)
+#     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.role}, ({self.user.username})"
+#     objects = CustomUserManager()
+
+#     def __str__(self):
+#         return self.username
+
+
+# class UserProfile(models.Model):
+#     ROLE_CHOICES = [
+#         ('Admin', 'Admin'),
+#         ('Librarian', 'Librarian'),
+#         ('Member', 'Member'),
+#     ]
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+#     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+#     def __str__(self):
+#         return f"{self.role}, ({self.user.username})"
